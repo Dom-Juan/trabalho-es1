68c447ae1a4c8b2f0873a077bfb2e6de02e2ecab
Tilteee <carlos.silva-filho@unesp.br>
2021-08-14 13:10:40 -0300

    Config backend

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,10 +1,46 @@
 const { Router } = require('express');
-
+const bcrypt = require('bcrypt');
 const routes = Router();
+
+const users = []
 
 routes.get('/', (req, res) => {
   return res.json({ msg: 'Hello World' });
 });
 
+// routes.post('/login', (req, res) => {
+//   return res.json({ msg: 'Hello World' });
+// });
+
+routes.get('/users', (req, res) => {
+  res.json(users);
+})
+
+routes.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send('Usuário Cadastrado com sucesso!')
+  } catch (er) {
+    res.status(500).send(er)
+  }
+})
+
+routes.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Não foi possível encontrar o usuário')
+  }
+  try {
+    if(await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
+    }
+  } catch {
+    res.status(500).send()
+  }
+})
 
 module.exports = routes;
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,5 +1,25 @@
 const app = require ('./app');
+const mysql = require('mysql');
+const db = require('./config/database')
+
+console.log(db.host);
+var mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "es1",
+  multipleStatements: true
+})
+
+mysqlConnection.connect((err) => {
+  if(!err){
+    console.log("Conectado");
+  }else{
+    console.log("Desconectado");
+  }
+})
 
 app.listen(3333, () => {
   console.log('Porta de acesso 3333');
 });
+
